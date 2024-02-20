a421e97b220cc917ac7ebb23a2f8ded7a63ba75d
L <luenwarneke@gmail.com>
2024-02-20 11:31:30 +1000

    Update dependencies and escape HTML in ListItem component

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import escape from "escape-html"
 
 type Props = {
     rogaine: Rogaine
@@ -9,7 +10,7 @@ export default function ListItem({ rogaine }: Props) {
 
     return (
         <li className="mt-4 text-2xl dark:text-white/90">
-            <Link className="underline hover:text-black/70 dark:hover:text-white" href={`/rogaines/${id}`}>{title}</Link>
+            <Link className="underline hover:text-black/70 dark:hover:text-white" href={`/rogaines/${escape(id)}`}>{title}</Link>
             <br />
             <p className="text-sm mt-1">{coordinates}</p>
         </li>
